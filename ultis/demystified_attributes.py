306d0b177db5a1c99fc2a3469559800d6711b53c
BinhMinhs10 <nguyenbinhminh07101997@gmail.com>
2020-11-18 10:21:26 +0700

    add func str, enter, exit

--- ultis/demystified_attributes.py
+++ ultis/demystified_attributes.py
@@ -11,6 +11,23 @@ class Car(Vehicle):
         # protected attributes
         self._country = country
 
+    def __str__(self):
+        return f"Car: {self.__horn}, {self._country!r}"
+
+    def _move_to_center(self):
+        print(f"The car ({self}) exhibit spot.")
+
+    def _move_to_side(self):
+        print(f"Move {self} back.")
+
+    def __enter__(self):
+        print("__enter__ is called")
+        self._move_to_center()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("__exit__ is called")
+        self._move_to_side()
+
 
 if __name__ == "__main__":
     v1 = Vehicle("honda", "jp")
@@ -21,3 +38,7 @@ if __name__ == "__main__":
     # alter value of private attr
     v2._Car__horn = 'new car'
     print(v2.__dict__)
+
+    print(v2)
+    with v2:
+        print("It's very good car")
