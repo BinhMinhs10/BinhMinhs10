216699c4255a0495ccd2355bcb70fe88215c2557
BinhMinhs10 <nguyenbinhminh07101997@gmail.com>
2021-03-23 09:06:19 +0700

    progress bar

--- ultis/protocols.py
+++ ultis/protocols.py
@@ -1,4 +1,5 @@
 from typing import Iterator, Iterable, Optional
+from tqdm import tqdm
 
 
 class IntList:
@@ -20,4 +21,8 @@ def print_numbered(items: Iterable[int]) -> None:
 
 x = IntList(3, IntList(5, None))
 print_numbered(x)  # OK
+
+print(10 * "=")
+for x in tqdm(range(1000000)):
+    pass
 print_numbered([4, 5])
